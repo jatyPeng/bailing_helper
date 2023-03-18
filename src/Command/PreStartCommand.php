d02ec0bf0249004dc6a1cf5954d068a6abb834d1
jaty <jaty@shequ.life>
2023-03-18 16:30:42 +0800

    增加preStart命令

---
+++ src/Command/PreStartCommand.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Kuaijing Bailing.
+ *
+ * @link     https://www.kuaijingai.com
+ * @document https://help.kuaijingai.com
+ * @contact  www.kuaijingai.com 7*12 9:00-21:00
+ */
+namespace Bailing\Command;
+
+use Hyperf\Command\Annotation\Command;
+use Hyperf\Command\Command as HyperfCommand;
+
+#[Command]
+class PreStartCommand extends HyperfCommand
+{
+    public function __construct()
+    {
+        parent::__construct('preStart');
+    }
+
+    public function handle()
+    {
+        $this->call('migrate');
+    }
+}
